a2fa6d84c4f7e409fd41202baa6a0c764e99f30b
yuliys99 <yuliys0511@gmail.com>
2020-10-21 09:42:17 +0700

    new project

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	// phpinfo();
+	// echo dan print
+	$name= "Yuliyati Salamah";
+	echo "My name is $name <br>";
+	echo "POLITEKNIK NEGERI BANYUWANGI";
+	 ?>
+</body>
+</html>
